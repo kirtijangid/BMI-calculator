041c214e6269f59f4835833a7658edf7a9ebb5a1
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-21 05:56:10 +0530

    adding textfield for height with input action

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Appbar extends StatelessWidget {
+
+class Appbar extends StatefulWidget {
+  const Appbar({super.key});
+
+  @override
+  State<Appbar> createState() => _AppbarState();
+}
+
+class _AppbarState extends State<Appbar> {
+   final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +21,24 @@ class Appbar extends StatelessWidget {
         shadowColor: const Color.fromARGB(255, 80, 27, 215),
         title: const Text("BMI calculator"),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+      child:
+       Column(
+        children: [
+            TextField(
+              controller: _heightController,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                labelText: 'Height (cm)',
+                suffixText: 'centmeters'
+              ),
+            )
+      ],
+      )
+      ,),
+
     );
   }
 }
