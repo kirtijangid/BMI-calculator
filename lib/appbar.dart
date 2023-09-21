bb248ba16d4ebbb62ef097ab9b7be5935a6ecf2d
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-21 06:21:40 +0530

    adding elevated button for calculation

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Appbar extends StatefulWidget {
   const Appbar({super.key});
 
@@ -9,48 +8,58 @@ class Appbar extends StatefulWidget {
 }
 
 class _AppbarState extends State<Appbar> {
-   final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
   final TextEditingController _weightController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        shadowColor: const Color.fromARGB(255, 80, 27, 215),
-        title: const Text("BMI calculator"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-      child:
-       Column(
-        children: [
-            TextField(
-              controller: _heightController,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                labelText: 'Height (cm)',
-                suffixText: 'centmeters'
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _heightController,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                labelText: 'Weight (kg)',
-                suffixText: 'kilograms'
-              ),
-            )
-      ],
-      ),
-      ),
+  double? bmi;
+  String errorText = '';
+  String status = '';
 
-    );
+  void calculateBMI() {
+    final double? height = double.tryParse(_heightController.value.text);
+    final double? weight = double.tryParse(_weightController.value.text);
   }
-}
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          shadowColor: const Color.fromARGB(255, 80, 27, 215),
+          title: const Text("BMI calculator"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _heightController,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: 'Height (cm)', suffixText: 'centmeters'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _heightController,
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                    labelText: 'Weight (kg)', suffixText: 'kilograms'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                ElevatedButton(
+                    onPressed: calculateBMI, child: const Text('Calculate'))
+              ])
+            ],
+          ),
+        ),
+      );
+    }
+  }
+
