8480315032e53b19e31fc379bbebe69086ebc1d8
kirtijangid <97035935+kirtijangid@users.noreply.github.com>
2023-09-21 06:54:27 +0530

    creating a row container for displaying result

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -117,7 +117,24 @@ class _AppbarState extends State<Appbar> {
                   SizedBox(
                     height: 50,
                   ),
-                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(bmi==null? '00.00' : bmi!.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 60,
+                        color: 
+                    status == 'Underweight' ? Colors.blue
+                                    : status == 'Normal weight' ? Colors.green
+                                    : status == 'Pre-Obesity' ? Colors.yellow.shade700
+                                    : status == 'Obesity class 1' ? Colors.orange
+                                    : status == 'Obesity class 2' ? Colors.deepOrangeAccent
+                                    : status == 'Obesity class 3' ? Colors.red
+                                    : null
+                      ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
